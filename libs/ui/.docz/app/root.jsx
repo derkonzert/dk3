365463a10874e33d954ecfedfc7bae8176e16c67
Julian Kempff <ju@formundcode.de>
2019-01-07 17:52:57 +0100

    fix(docz): Removes .docz folder from respository

--- libs/ui/.docz/app/root.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { hot } from 'react-hot-loader'
-import Theme from 'docz-theme-default'
-
-import Wrapper from 'utils/doczWrapper'
-
-const Root = () => <Theme wrapper={Wrapper} />
-
-export default hot(module)(Root)
