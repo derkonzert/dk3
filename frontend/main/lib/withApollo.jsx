f4203cc4d77e67b6e7a86a6cffc312c088d5921b
Julian Kempff <ju@formundcode.de>
2019-01-20 19:00:39 +0100

    chore(coverage): Adds frontend directory to coverage collection

--- frontend/main/lib/withApollo.jsx
+++ frontend/main/lib/withApollo.jsx
@@ -17,73 +17,7 @@ function getAccessToken(req, options = {}) {
 }
 
 export default App => {
-  return class WithData extends React.Component {
-    static displayName = `WithData(${App.displayName})`
-    static propTypes = {
-      apolloState: PropTypes.object.isRequired,
-    }
-
-    static async getInitialProps(ctx) {
-      const {
-        Component,
-        router,
-        ctx: { req, res },
-      } = ctx
-      const apollo = initApollo(
-        {},
-        {
-          getToken: () => getAccessToken(req),
-        }
-      )
-
-      ctx.ctx.apolloClient = apollo
-
-      let appProps = {}
-      if (App.getInitialProps) {
-        appProps = await App.getInitialProps(ctx)
-      }
-
-      if (res && res.finished) {
-        // When redirecting, the response is finished.
-        // No point in continuing to render
-        return {}
-      }
-
-      if (!process.browser) {
-        // Run all graphql queries in the component tree
-        // and extract the resulting data
-        try {
-          // Run all GraphQL queries
-          /* TODO: reenable serverside gql data fetching */
-          // await getDataFromTree(
-          //   <App
-          //     {...appProps}
-          //     Component={Component}
-          //     router={router}
-          //     apolloClient={apollo}
-          //   />
-          // )
-        } catch (error) {
-          // Prevent Apollo Client GraphQL errors from crashing SSR.
-          // Handle them in components via the data.error prop:
-          // https://www.apollographql.com/docs/react/api/react-apollo.html#graphql-query-data-error
-          console.error("Error while running `getDataFromTree`", error)
-        }
-
-        // getDataFromTree does not call componentWillUnmount
-        // head side effect therefore need to be cleared manually
-        Head.rewind()
-      }
-
-      // Extract query data from the Apollo's store
-      const apolloState = apollo.cache.extract()
-
-      return {
-        ...appProps,
-        apolloState,
-      }
-    }
-
+  class WithData extends React.Component {
     constructor(props) {
       super(props)
       // `getDataFromTree` renders the component first, the client is passed off as a property.
@@ -99,4 +33,72 @@ export default App => {
       return <App {...this.props} apolloClient={this.apolloClient} />
     }
   }
+
+  WithData.displayName = `WithData(${App.displayName})`
+  WithData.propTypes = {
+    apolloState: PropTypes.object.isRequired,
+  }
+
+  WithData.getInitialProps = async ctx => {
+    const {
+      Component,
+      router,
+      ctx: { req, res },
+    } = ctx
+    const apollo = initApollo(
+      {},
+      {
+        getToken: () => getAccessToken(req),
+      }
+    )
+
+    ctx.ctx.apolloClient = apollo
+
+    let appProps = {}
+    if (App.getInitialProps) {
+      appProps = await App.getInitialProps(ctx)
+    }
+
+    if (res && res.finished) {
+      // When redirecting, the response is finished.
+      // No point in continuing to render
+      return {}
+    }
+
+    if (!process.browser) {
+      // Run all graphql queries in the component tree
+      // and extract the resulting data
+      try {
+        // Run all GraphQL queries
+        /* TODO: reenable serverside gql data fetching */
+        // await getDataFromTree(
+        //   <App
+        //     {...appProps}
+        //     Component={Component}
+        //     router={router}
+        //     apolloClient={apollo}
+        //   />
+        // )
+      } catch (error) {
+        // Prevent Apollo Client GraphQL errors from crashing SSR.
+        // Handle them in components via the data.error prop:
+        // https://www.apollographql.com/docs/react/api/react-apollo.html#graphql-query-data-error
+        console.error("Error while running `getDataFromTree`", error)
+      }
+
+      // getDataFromTree does not call componentWillUnmount
+      // head side effect therefore need to be cleared manually
+      Head.rewind()
+    }
+
+    // Extract query data from the Apollo's store
+    const apolloState = apollo.cache.extract()
+
+    return {
+      ...appProps,
+      apolloState,
+    }
+  }
+
+  return WithData
 }
