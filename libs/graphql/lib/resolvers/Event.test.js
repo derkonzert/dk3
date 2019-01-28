eb74bdd5a5ea7fae48fb0bf5975358997f802a64
Julian Kempff <me@ju.am>
2019-01-28 21:19:01 +0100

    test(coverage): Increases coverage on resolver tests and ui Button tests

--- libs/graphql/lib/resolvers/Event.test.js
+++ libs/graphql/lib/resolvers/Event.test.js
@@ -1,4 +1,4 @@
-const { Event } = require("../../lib/resolvers/Event")
+const { Event } = require("./Event")
 
 describe("Event", () => {
   describe("fancyness", () => {
@@ -12,6 +12,36 @@ describe("Event", () => {
       expect(one).toBe(1)
       expect(alsoOne).toBe(1)
       expect(zero).toBe(0)
+    })
+  })
+
+  describe("bookmarkedByMe", () => {
+    it("resolves to false if there is no user", () => {
+      expect.assertions(1)
+
+      return expect(
+        Event.bookmarkedByMe(undefined, undefined, {})
+      ).resolves.toBe(false)
+    })
+
+    it("resolves to false if the users ID doesnt appear in bookmarkedBy", () => {
+      expect.assertions(1)
+
+      return expect(
+        Event.bookmarkedByMe({ bookmarkedBy: [] }, undefined, {
+          user: { _id: "1234" },
+        })
+      ).resolves.toBe(false)
+    })
+
+    it("resolves to true if the users ID does appear in bookmarkedBy", () => {
+      expect.assertions(1)
+
+      return expect(
+        Event.bookmarkedByMe({ bookmarkedBy: ["1234"] }, undefined, {
+          user: { _id: "1234" },
+        })
+      ).resolves.toBe(true)
     })
   })
 })
--- libs/ui/form/Button.test.jsx
+++ libs/ui/form/Button.test.jsx
@@ -2,12 +2,26 @@ import React from "react"
 import { matchers } from "jest-emotion"
 import { mount } from "enzyme"
 
-import { Button } from "./Button"
+import { Button, FancyButton, VeryFancyButton } from "./Button"
 
 expect.extend(matchers)
 
-describe("Button", () => {
-  it("mounts without throwing", () => {
-    expect(() => mount(<Button />)).not.toThrow()
+describe("Buttons", () => {
+  describe("normal Button", () => {
+    it("mounts without throwing", () => {
+      expect(() => mount(<Button />)).not.toThrow()
+    })
+  })
+
+  describe("FancyButton", () => {
+    it("mounts without throwing", () => {
+      expect(() => mount(<FancyButton />)).not.toThrow()
+    })
+  })
+
+  describe("VeryFancyButton", () => {
+    it("mounts without throwing", () => {
+      expect(() => mount(<VeryFancyButton />)).not.toThrow()
+    })
   })
 })
