b435dae55b7d495adbc197b05bcfa06b093313fd
jkempff <kempff@ju.am>
2019-03-20 18:00:13 +0100

    feat(ui): Add userscalable property to viewport setting

--- frontend/main/pages/_document.jsx
+++ frontend/main/pages/_document.jsx
@@ -16,7 +16,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1.0"
+            content="width=device-width, initial-scale=1.0, user-scalable=no"
           />
         </Head>
         <body>
