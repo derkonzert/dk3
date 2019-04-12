079ac36e5e9cd159e95dd3a0f819372b4607ec4b
jkempff <kempff@ju.am>
2019-04-12 19:19:13 +0200

    chore(ci): forward all options for exec

--- scripts/start-ci/lib/execPromised.js
+++ scripts/start-ci/lib/execPromised.js
@@ -10,25 +10,19 @@ exports.killAll = () => {
   }
 }
 
-exports.execPromised = (command, { cwd }) => {
+exports.execPromised = (command, options) => {
   return new Promise((resolve, reject) => {
     var exec = require("child_process").exec
 
-    const cmd = exec(
-      command,
-      {
-        cwd: cwd,
-      },
-      function(error, stdout, stderr) {
-        // work with result
-        if (error) {
-          return reject(error)
-        }
-
-        logger("stdout:", stdout)
-        logger("stderr:", stderr)
+    const cmd = exec(command, options, function(error, stdout, stderr) {
+      // work with result
+      if (error) {
+        return reject(error)
       }
-    )
+
+      logger("stdout:", stdout)
+      logger("stderr:", stderr)
+    })
 
     childProcesses.push(cmd)
   })
