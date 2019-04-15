dbb0cfeb75abccb1bea31f46916e2038223c2010
jkempff <kempff@ju.am>
2019-04-15 23:02:14 +0100

    fix: increase maxBuffer to fix integration tests on ci

--- scripts/start-ci/lib/start-ci.js
+++ scripts/start-ci/lib/start-ci.js
@@ -3,6 +3,8 @@ const { override } = require("@dk3/config")
 
 const { execPromised } = require("./execPromised")
 
+const TEN_MEGA_BYTE = 1024 * 1024 * 10
+
 module.exports = async function startCi() {
   const seed = require("@dk3/seed/lib/seed")
 
@@ -21,11 +23,11 @@ module.exports = async function startCi() {
     await Promise.all([
       execPromised(`MONGODB_URI=${uriString} yarn dev`, {
         cwd: path.resolve(__dirname, "../../../"),
-        maxBuffer: 1024 * 500,
+        maxBuffer: TEN_MEGA_BYTE,
       }),
       execPromised(`MONGODB_URI=${uriString} yarn dev`, {
         cwd: path.resolve(__dirname, "../../../frontend/main"),
-        maxBuffer: 1024 * 500,
+        maxBuffer: TEN_MEGA_BYTE,
       }),
     ])
 
