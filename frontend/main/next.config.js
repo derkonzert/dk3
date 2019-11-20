f673ac7c5ab79058966755cd9023982eadbbd38f
jkempff <kempff@ju.am>
2019-11-20 22:43:19 +0100

    fix: try exporting index.html files only

--- frontend/main/next.config.js
+++ frontend/main/next.config.js
@@ -5,6 +5,8 @@ const sharedWebpack = require("@dk3/shared-frontend/next.webpack.config")
 module.exports = withCSS({
   target: "serverless",
 
+  exportTrailingSlash: true,
+
   env: {
     SENTRY_DSN_FRONTEND: process.env.SENTRY_DSN_FRONTEND,
   },
