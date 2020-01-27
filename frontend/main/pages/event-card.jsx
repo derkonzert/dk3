5b6e3063677c921ab0d38f3f83c1141c832c7328
jkempff <kempff@ju.am>
2020-01-27 22:12:06 +0100

    fix: extract id from path when not available

--- frontend/main/pages/event-card.jsx
+++ frontend/main/pages/event-card.jsx
@@ -2,8 +2,21 @@ import { withRouter } from "next/router"
 
 import { EventCardQuery } from "../components/event-detail/EventCard"
 
+const extractIdFromPath = path => {
+  const match = path.match(/\/event\/(.)*-(?<eventId>.*)$/)
+  if (match) {
+    return match[2]
+  } else {
+    return undefined
+  }
+}
+
 const EventPage = function Event({ router }) {
-  return <EventCardQuery id={router.query.eventId} />
+  const eventId = !router.query.eventId
+    ? extractIdFromPath(router.asPath)
+    : router.query.eventId
+
+  return <EventCardQuery id={eventId} />
 }
 
 export default withRouter(EventPage)
